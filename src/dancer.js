b5e23290d44e5296a160e5255a23bf83804a4c8e
andyderek <andielane@yahoo.com>
2016-03-21 19:57:02 -0700

    working through inheritance. we keep getting error message "function is undefined," but haven't made much headway on figuring out why this is happening.

--- src/dancer.js
+++ src/dancer.js
@@ -1,30 +1,36 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps){
-  this.setPosition(top, left)
-  this.timeBetweenSteps = timeBetweenSteps;
+var Dancer = function(top, left, timeBetweenSteps){
+  this.setPosition(top, left);
+  this._timeBetweenSteps = timeBetweenSteps;
   this.$node = $('<span class="dancer"></span>');
-  this.step = step();
+  this.step();
 };
 
   // use jQuery to create an HTML <span> tag
+// Dancer.prototype.constructor = Dancer;
 
+Dancer.prototype.step = function(){
+  var that = this;
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(function(){
+    that.step()
+  }, this._timeBetweenSteps);
+  console.log('step')
+};
 
-  Dancer.prototype.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, this.timeBetweenSteps);
+Dancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  // console.log(this.$node)
+  var styleSettings = {
+    top: top,
+    left: left
   };
 
-  Dancer.prototype.setPosition = function(top, left){
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
-  };
+  // this.$node.css(styleSettings);
+};
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
